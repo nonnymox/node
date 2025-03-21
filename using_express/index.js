ef18f2195d95073b0465b4b4cec4b6a4c5f41af1
nonnymox <knonxho@gmail.com>
2025-03-21 06:22:26 -0700

    Added listen and response methods to app

--- using_express/index.js
+++ using_express/index.js
@@ -5,3 +5,14 @@
 // Write Server Application
 // Start Server
 
+import express from "express";
+const app = express();
+const port = 3000;
+// Using the Get Request
+app.get("/", (req, resp) => {
+  resp.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
